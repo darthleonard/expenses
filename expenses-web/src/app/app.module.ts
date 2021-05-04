29521335adf81e0519e2b8879572c29b20e82abf
darthleonard <leolinuxmx@gmail.com>
2021-05-03 22:16:00 -0500

    angular project created

---
+++ expenses-web/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GridModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
